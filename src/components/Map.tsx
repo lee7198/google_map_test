8ca51b8d070a0cb939a6b5084022b7ac7a4e9be4
lee7198 <lee7198@naver.com>
2023-02-25 20:17:15 +0900

    install google map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,24 @@
-import { GoogleMap } from "@react-google-maps/api";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
 export const Map = () => {
-  return <GoogleMap></GoogleMap>;
+  const api_key = import.meta.env.VITE_MAP_API_KEY;
+
+  const containerStyle = {
+    width: "100%",
+    height: "400px",
+  };
+
+  const center = {
+    lat: 37.5665,
+    lng: 126.978,
+  };
+
+  console.log(api_key);
+  return (
+    <LoadScript googleMapsApiKey={import.meta.env.VITE_MAP_API_KEY}>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+        {/* 마커, 폴리곤 등 구현 */}
+      </GoogleMap>
+    </LoadScript>
+  );
 };
